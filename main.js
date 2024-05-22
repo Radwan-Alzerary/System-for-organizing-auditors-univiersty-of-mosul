b6408a4a0f50a39364ba4e831270b659d234da08
radwan <rdoaniv@ymail.com>
2024-05-22 13:50:36 +0300

    v0.005

--- main.js
+++ main.js
@@ -72,13 +72,13 @@ const browserPromise = puppeteer.launch(); // Launch the browser once
 async function printImageAsync(imagePath, printincount) {
   const printer = new ThermalPrinter({
     type: PrinterTypes.EPSON,
-    interface: `tcp://172.20.82.221//:9100`,
+    interface: `tcp://172.20.82.221/:9100`,
     // characterSet: CharacterSet.SLOVENIA,
     removeSpecialCharacters: false,
     lineCharacter: "=",
     breakLine: BreakLine.WORD,
     options: {
-      timeout: 1000,
+      timeout: 4000,
     },
   });
   try {
